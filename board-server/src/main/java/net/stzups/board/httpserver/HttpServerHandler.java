348ef461f60da13f196237590ddb8d684e850c53
stzups <lemonpickles1558@gmail.com>
2020-12-18 11:27:14 -0500

    simplified 404 page

--- board-server/src/main/java/net/stzups/board/httpserver/HttpServerHandler.java
+++ board-server/src/main/java/net/stzups/board/httpserver/HttpServerHandler.java
@@ -214,7 +214,7 @@ public class HttpServerHandler extends SimpleChannelInboundHandler<FullHttpReque
 
     private void sendError(ChannelHandlerContext ctx, HttpResponseStatus status) {
         FullHttpResponse response = new DefaultFullHttpResponse(
-                HttpVersion.HTTP_1_1, status, Unpooled.copiedBuffer("Failure: " + status + "\r\n", CharsetUtil.UTF_8));
+                HttpVersion.HTTP_1_1, status, Unpooled.copiedBuffer(status + "\r\n", CharsetUtil.UTF_8));
         response.headers().set(HttpHeaderNames.CONTENT_TYPE, "text/plain; charset=UTF-8");
 
         sendAndCleanupConnection(ctx, response);
