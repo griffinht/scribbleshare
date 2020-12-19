96389c468edf68788f8032e839d4e98f55dca3ac
stzups <lemonpickles1558@gmail.com>
2020-12-19 00:02:04 -0500

    refactor logging

--- board-server/src/main/java/net/stzups/board/server/ServerInitializer.java
+++ board-server/src/main/java/net/stzups/board/server/ServerInitializer.java
@@ -8,10 +8,12 @@ import io.netty.handler.codec.http.HttpServerCodec;
 import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
 import io.netty.handler.codec.http.websocketx.extensions.compression.WebSocketServerCompressionHandler;
 import io.netty.handler.stream.ChunkedWriteHandler;
+import net.stzups.board.Board;
 
 public class ServerInitializer extends ChannelInitializer<SocketChannel> {
     @Override
     protected void initChannel(SocketChannel socketChannel) {
+        Board.getLogger().info("New connection from " + socketChannel.remoteAddress());
         ChannelPipeline pipeline = socketChannel.pipeline();
         //todo ssl
         pipeline.addLast(new HttpServerCodec());
--- board-server/src/main/java/net/stzups/board/server/WebSocketFrameHandler.java
+++ board-server/src/main/java/net/stzups/board/server/WebSocketFrameHandler.java
@@ -4,12 +4,16 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
 import io.netty.handler.codec.http.websocketx.WebSocketFrame;
+import net.stzups.board.Board;
 
 public class WebSocketFrameHandler extends SimpleChannelInboundHandler<WebSocketFrame> {
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, WebSocketFrame frame) {
         if (frame instanceof TextWebSocketFrame) {
-            ctx.channel().writeAndFlush(new TextWebSocketFrame(((TextWebSocketFrame) frame).text()));
+            TextWebSocketFrame textWebSocketFrame = (TextWebSocketFrame) frame;
+            String text = textWebSocketFrame.text();
+            Board.getLogger().info(ctx.channel().remoteAddress() + " sent " + text);
+            ctx.channel().writeAndFlush(new TextWebSocketFrame(text));
         } else {
             throw new UnsupportedOperationException("Unsupported frame type " + frame.getClass().getName());
         }
