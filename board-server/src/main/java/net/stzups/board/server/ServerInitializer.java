c4752bfd523d1e54f85de3b3ec575349f3575c6d
stzups <lemonpickles1558@gmail.com>
2020-12-20 23:51:10 -0500

    add traffic monitor

--- board-server/src/main/java/net/stzups/board/server/ServerInitializer.java
+++ board-server/src/main/java/net/stzups/board/server/ServerInitializer.java
@@ -8,10 +8,14 @@ import io.netty.handler.codec.http.HttpServerCodec;
 import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
 import io.netty.handler.codec.http.websocketx.extensions.compression.WebSocketServerCompressionHandler;
 import io.netty.handler.stream.ChunkedWriteHandler;
+import io.netty.handler.traffic.GlobalTrafficShapingHandler;
+import io.netty.handler.traffic.TrafficCounter;
 import net.stzups.board.Board;
 import net.stzups.board.room.PacketHandler;
 import net.stzups.board.protocol.PacketEncoder;
 import net.stzups.board.protocol.PacketDecoder;
+
+import java.util.concurrent.Executors;
 
 /**
  * Creates pipeline to handle HTTP requests and WebSocket connections on the same port
@@ -19,14 +23,25 @@ import net.stzups.board.protocol.PacketDecoder;
  * Connections not made to the WebSocket path go to ServerHandler
  */
 public class ServerInitializer extends ChannelInitializer<SocketChannel> {
+    private GlobalTrafficShapingHandler globalTrafficShapingHandler;
     private PacketEncoder packetEncoder = new PacketEncoder();
     private PacketDecoder packetDecoder = new PacketDecoder();
+
+    ServerInitializer() {
+        globalTrafficShapingHandler = new GlobalTrafficShapingHandler(Executors.newSingleThreadScheduledExecutor(), 0, 0, 1000) {
+            @Override
+            protected void doAccounting(TrafficCounter counter) {
+                System.out.print("\rread " + (double) counter.lastReadThroughput() / 1000 * 8 + "kb/s, write "  + (double) counter.lastWriteThroughput() / 1000 * 8 + "kb/s");
+            }
+        };
+    }
 
     @Override
     protected void initChannel(SocketChannel socketChannel) {
         Board.getLogger().info("New connection from " + socketChannel.remoteAddress());
         ChannelPipeline pipeline = socketChannel.pipeline();
         //todo ssl
+        pipeline.addLast(globalTrafficShapingHandler);
         pipeline.addLast(new HttpServerCodec());
         pipeline.addLast(new HttpObjectAggregator(65536));
         pipeline.addLast(new ChunkedWriteHandler());
