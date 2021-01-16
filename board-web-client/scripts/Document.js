a1dbf05dc2961cbc449b98480a907e938bcb5ecf
stzups <lemonpickles1558@gmail.com>
2021-01-16 18:05:03 -0500

    refactor document.js layout

--- board-web-client/scripts/Document.js
+++ board-web-client/scripts/Document.js
@@ -6,68 +6,8 @@ import SidebarItem from './SidebarItem.js';
 import Client from './Client.js'
 import socket from './WebSocketHandler.js'
 
-const clients = new Map();
-const localClient = new LocalClient();
-var activeDocument;
-
-{
-    window.addEventListener('resize', resizeCanvas);
-    function resizeCanvas() {
-        let imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-        canvas.width = canvas.parentElement.offsetWidth;
-        canvas.height = canvas.parentElement.offsetHeight;
-        ctx.putImageData(imageData, 0, 0);
-        //todo redraw?
-    };
-    resizeCanvas();
-}
-
-var last = performance.now();
-function draw(now) {
-    let dt = (now - last);
-    last = now;
-
-    activeDocument.draw(dt);
-
-    window.requestAnimationFrame(draw);
-}
-
-var next = 0;
-function newDocument() {
-    new Document('Untitled ' + next++);
-}
-
-document.getElementById('add').addEventListener('click', newDocument);
-
-socket.addEventListener('addclient', (event) => {
-    let client = new Client(event.id);
-    clients.set(client.id, client);
-    console.log('Add client ', client);
-});
-socket.addEventListener('removeclient', (event) => {
-    console.log('Remove client ', clients.delete(event.id));
-});
-socket.addEventListener('draw', (event) => {
-    let client = clients.get(event.id);
-    event.points.forEach(point => {//todo styling (point) => {} or point => {}
-        client.points.push(point);
-    });
-});
-socket.addEventListener('open', (event) => {
-    let doc = new Document(event.id, event.name);
-    //window.history.pushState(doc.name, document.title, '/d/' + id);
-    Board.inviteButton.innerHTML = 'invite';//todo abstract invitebutton to class???????
-    var invite = document.location.href.substring(document.location.href.lastIndexOf("/") + 1);
-    if (invite === '') {
-        socket.sendCreate();
-    } else {
-        socket.sendOpen(invite);
-    }
-
-});
-
-
 const documents = new Map();
+var activeDocument = null;
 
 export default class Document {
     constructor(name, id, points) {
@@ -82,9 +22,73 @@ export default class Document {
 
     open() {
         activeDocument = this;
+        socket.sendOpen(this.id);
     }
 
     draw(dt) {
         console.log('drawing ');
     }
 }
+
+var next = 0;
+function newDocument() {
+    return new Document('Untitled ' + next++);
+}
+
+document.getElementById('add').addEventListener('click', newDocument);
+
+window.addEventListener('resize', resizeCanvas);
+function resizeCanvas() {
+    let imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    canvas.width = canvas.parentElement.offsetWidth;
+    canvas.height = canvas.parentElement.offsetHeight;
+    ctx.putImageData(imageData, 0, 0);
+    //todo redraw?
+};
+resizeCanvas();
+
+var last = performance.now();
+function draw(now) {
+    let dt = (now - last);
+    last = now;
+
+    if (activeDocument != null) {
+        activeDocument.draw(dt);
+    }
+
+    window.requestAnimationFrame(draw);
+}
+window.requestAnimationFrame(draw);
+
+const clients = new Map();
+socket.addEventListener('addclient', (event) => {
+    let client = new Client(event.id);
+    clients.set(client.id, client);
+    console.log('Add client ', client);
+});
+socket.addEventListener('removeclient', (event) => {
+    console.log('Remove client ', clients.delete(event.id));
+});
+socket.addEventListener('draw', (event) => {
+    let client = clients.get(event.id);
+    event.points.forEach(point => {//todo styling (point) => {} or point => {}
+        client.points.push(point);
+    });
+});
+console.log(socket);
+socket.addEventListener('open', (event) => {
+    let doc = new Document(event.name, event.id);
+    //window.history.pushState(doc.name, document.title, '/d/' + id);
+});
+socket.addEventListener('socketopen', (event) => {
+    Board.inviteButton.innerHTML = 'invite';//todo abstract invitebutton to class???????
+    var invite = document.location.href.substring(document.location.href.lastIndexOf("/") + 1);
+    if (invite === '') {
+        socket.sendCreate();
+    } else {
+        socket.sendOpen(invite);
+    }
+});
+
+activeDocument = newDocument();
+const localClient = new LocalClient();
