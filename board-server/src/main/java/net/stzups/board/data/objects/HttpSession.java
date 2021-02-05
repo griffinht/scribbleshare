8ee03f212e2473abd7bfebb39a9246a58f1d9bc4
stzups <lemonpickles1558@gmail.com>
2021-02-04 23:04:38 -0500

    unused import

--- board-server/src/main/java/net/stzups/board/data/objects/HttpSession.java
+++ board-server/src/main/java/net/stzups/board/data/objects/HttpSession.java
@@ -11,9 +11,7 @@ import java.net.InetAddress;
 import java.security.SecureRandom;
 import java.util.Base64;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
