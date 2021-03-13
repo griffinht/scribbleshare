e336287e1b6791f54c2c374603b9339fcae8e581
stzups <lemonpickles1558@gmail.com>
2021-03-13 15:33:26 -0500

    simplify protocol organization

--- board-room/src/main/java/net/stzups/board/server/websocket/protocol/server/messages/ServerMessageAddClient.java
+++ board-room/src/main/java/net/stzups/board/server/websocket/protocol/server/messages/ServerMessageAddClient.java
@@ -2,20 +2,21 @@ package net.stzups.board.server.websocket.protocol.server.messages;
 
 import io.netty.buffer.ByteBuf;
 import net.stzups.board.server.websocket.Client;
-import net.stzups.board.server.websocket.protocol.server.ServerMessageClient;
+import net.stzups.board.server.websocket.protocol.server.ServerMessage;
 import net.stzups.board.server.websocket.protocol.server.ServerMessageType;
 
-public class ServerMessageAddClient extends ServerMessageClient {
+public class ServerMessageAddClient extends ServerMessage {
     private Client client;
 
     public ServerMessageAddClient(Client client) {
-        super(ServerMessageType.ADD_CLIENT, client);
+        super(ServerMessageType.ADD_CLIENT);
         this.client = client;
     }
 
     @Override
     public void serialize(ByteBuf byteBuf) {
         super.serialize(byteBuf);
+        byteBuf.writeShort(client.getId());
         byteBuf.writeLong(client.getUser().getId());
     }
 }
--- board-room/src/main/java/net/stzups/board/server/websocket/protocol/server/messages/ServerMessageAddUser.java
+++ board-room/src/main/java/net/stzups/board/server/websocket/protocol/server/messages/ServerMessageAddUser.java
@@ -2,16 +2,20 @@ package net.stzups.board.server.websocket.protocol.server.messages;
 
 import io.netty.buffer.ByteBuf;
 import net.stzups.board.data.objects.User;
+import net.stzups.board.server.websocket.protocol.server.ServerMessage;
 import net.stzups.board.server.websocket.protocol.server.ServerMessageType;
-import net.stzups.board.server.websocket.protocol.server.ServerMessageUser;
 
-public class ServerMessageAddUser extends ServerMessageUser {
+public class ServerMessageAddUser extends ServerMessage {
+    private User user;
+
     public ServerMessageAddUser(User user) {
-        super(ServerMessageType.ADD_USER, user);
+        super(ServerMessageType.ADD_USER);
+        this.user = user;
     }
 
     @Override
     public void serialize(ByteBuf byteBuf) {
         super.serialize(byteBuf);
+        byteBuf.writeLong(user.getId());
     }
 }
