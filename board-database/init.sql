8d353da8a602c4a5dde29fbaef2f7321c8be5adb
stzups <lemonpickles1558@gmail.com>
2021-04-04 10:44:49 -0400

    add users table

--- board-database/init.sql
+++ board-database/init.sql
@@ -1,2 +1,11 @@
 CREATE USER board PASSWORD 'changeme';
 CREATE DATABASE board;
+
+-- Connect to board database (currently on default postgres database)
+\c board
+CREATE TABLE users(
+    id bigint NOT NULL,
+    owned_documents bytea[] NOT NULL,
+    shared_documents bytea[] NOT NULL,
+    PRIMARY KEY(id)
+)
