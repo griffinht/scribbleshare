183b01c9e6fcf4a73cd69a67dd0ff8d8ac2ccd88
stzups <lemonpickles1558@gmail.com>
2021-04-04 20:16:51 -0400

    probably clean up db code, doesn't work

--- board-room/src/main/java/net/stzups/board/data/database/postgres/PostgresDatabase.java
+++ board-room/src/main/java/net/stzups/board/data/database/postgres/PostgresDatabase.java
@@ -2,6 +2,7 @@ package net.stzups.board.data.database.postgres;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
+import net.stzups.board.BoardRoom;
 import net.stzups.board.data.database.Database;
 import net.stzups.board.data.objects.Document;
 import net.stzups.board.data.objects.PersistentUserSession;
@@ -33,8 +34,7 @@ public class PostgresDatabase implements Database {
 
     @Override
     public void addUser(User user) {
-        try {
-            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO users(id, owned_documents, shared_documents) VALUES (?, ?, ?)");
+        try (PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO users(id, owned_documents, shared_documents) VALUES (?, ?, ?)")) {
             preparedStatement.setLong(1, user.getId());
             preparedStatement.setArray(2, connection.createArrayOf("bigint", user.getOwnedDocuments().toArray()));
             preparedStatement.setArray(3, connection.createArrayOf("bigint", user.getSharedDocuments().toArray()));
@@ -47,31 +47,32 @@ public class PostgresDatabase implements Database {
 
     @Override
     public User getUser(long id) {
-        try {
-            User user = users.get(id);
-            if (user == null) {
-                PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM users WHERE id=?");
+        User user = users.get(id);
+        if (user == null) {
+            try (PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM users WHERE id=?")) {
                 preparedStatement.setLong(1, id);
                 ResultSet resultSet = preparedStatement.executeQuery();
-                if (!resultSet.next()) {
-                    System.out.println("user does not exist");
+                if (resultSet.next()) {
+                    user = new User(id,
+                            new ArrayList<>(Arrays.asList((Long[]) resultSet.getArray("owned_documents").getArray())),
+                            new ArrayList<>(Arrays.asList((Long[]) resultSet.getArray("shared_documents").getArray())));
+                    users.put(user.getId(), user);
+                } else {
+                    BoardRoom.getLogger().warning("User with id " + id + " does not exist");
                     return null;
                 }
-                user = new User(id, new ArrayList<>(Arrays.asList((Long[]) resultSet.getArray("owned_documents").getArray())), new ArrayList<>(Arrays.asList((Long[]) resultSet.getArray("shared_documents").getArray())));
-                users.put(user.getId(), user);
+            } catch (SQLException e) {
+                e.printStackTrace();
+                return null;
             }
-            return user;
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return null;
         }
+        return user;
     }
 
     @Override
     public Document createDocument(User owner) {
         Document document = new Document(owner);
-        try {
-            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO documents(id, owner, name) VALUES (?, ?, ?)");
+        try (PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO documents(id, owner, name) VALUES (?, ?, ?)")){
             preparedStatement.setLong(1, document.getId());
             preparedStatement.setLong(2, document.getOwner().getId());
             preparedStatement.setString(3, document.getName());
@@ -86,35 +87,36 @@ public class PostgresDatabase implements Database {
 
     @Override
     public Document getDocument(long id) {
-        try {
-            Document document = documents.get(id);
-            if (document == null) {
-                PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM documents WHERE id=?");
+        Document document = documents.get(id);
+        if (document == null) {
+            try (PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM documents WHERE id=?")) {
                 preparedStatement.setLong(1, id);
                 ResultSet resultSet = preparedStatement.executeQuery();
-                if (!resultSet.next()) {
-                    System.out.println("document does not exist");
+                if (resultSet.next()) {
+                    long userId = resultSet.getLong("owner");
+                    User user = getUser(userId);
+                    if (user == null) {
+                        BoardRoom.getLogger().warning("Document with id " + id + " has no owner with id " + userId);
+                        return null;
+                    }
+                    document = new Document(id, user, resultSet.getString("name"));
+                    documents.put(document.getId(), document);
+                } else {
+                    BoardRoom.getLogger().warning("Document with id " + id + " does not exist");
                     return null;
+
                 }
-                User user = getUser(resultSet.getLong("owner"));
-                if (user == null) {
-                    System.out.println("no owner for document");
-                    return null;
-                }
-                document = new Document(id, user, resultSet.getString("name"));
-                documents.put(document.getId(), document);
+            } catch (SQLException e) {
+                e.printStackTrace();
+                return null;
             }
-            return document;
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return null;
         }
+        return document;
     }
 
     @Override
     public Canvas getCanvas(Document document) {
-        try {
-            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM canvases WHERE document=?");
+        try (PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM canvases WHERE document=?")) {
             preparedStatement.setLong(1, document.getId());
             ResultSet resultSet = preparedStatement.executeQuery();
             if (resultSet.next()) {
@@ -124,14 +126,13 @@ public class PostgresDatabase implements Database {
             }
         } catch (SQLException | IOException e) {
             e.printStackTrace();
+            return null;//todo error handling??
         }
-        return null;//todo error handling??
     }
 
     @Override
     public void saveCanvas(Canvas canvas) {
-        try {
-            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO canvases(document, data) VALUES(?, ?) ON CONFLICT (document) DO UPDATE SET data=excluded.data");
+        try (PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO canvases(document, data) VALUES(?, ?) ON CONFLICT (document) DO UPDATE SET data=excluded.data")) {
             preparedStatement.setLong(1, canvas.getDocument().getId());
             ByteBuf byteBuf = Unpooled.buffer();
             canvas.serialize(byteBuf);
@@ -149,8 +150,7 @@ public class PostgresDatabase implements Database {
 
     @Override
     public void addUserSession(PersistentUserSession persistentUserSession) {
-        try {
-            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO persistent_user_sessions(id, \"user\", creation_time, hashed_token) VALUES (?, ?, ?, ?)");
+        try (PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO persistent_user_sessions(id, \"user\", creation_time, hashed_token) VALUES (?, ?, ?, ?)")) {
             preparedStatement.setLong(1, persistentUserSession.getId());
             preparedStatement.setLong(2, persistentUserSession.getUser());
             preparedStatement.setTimestamp(3, persistentUserSession.getCreation());
@@ -166,19 +166,27 @@ public class PostgresDatabase implements Database {
      */
     @Override
     public PersistentUserSession removeUserSession(long id) {
-        try {
-            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM persistent_user_sessions WHERE id=?");
+        PersistentUserSession persistentUserSession;
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM persistent_user_sessions WHERE id=?")) {
             preparedStatement.setLong(1, id);
             ResultSet resultSet = preparedStatement.executeQuery();
-            if (!resultSet.next()) {
+            if (resultSet.next()) {
+                persistentUserSession = new PersistentUserSession(id, resultSet.getLong("user"), resultSet.getTimestamp("creation_time"), resultSet.getBinaryStream("hashed_token").readAllBytes());
+            } else {
+                BoardRoom.getLogger().warning("PersistentUserSession with id " + id + " does not exist");
                 return null;
             }
-            PersistentUserSession persistentUserSession = new PersistentUserSession(id, resultSet.getLong("user"), resultSet.getTimestamp("creation_time"), resultSet.getBinaryStream("hashed_token").readAllBytes());
-            preparedStatement = connection.prepareStatement("DELETE FROM persistent_user_sessions WHERE id=?");
+        } catch (SQLException | IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM persistent_user_sessions WHERE id=?")) {
             preparedStatement.setLong(1, id);
             preparedStatement.execute();
             return persistentUserSession;
-        } catch (SQLException | IOException e) {
+        } catch (SQLException e) {
             e.printStackTrace();
             return null;
         }
