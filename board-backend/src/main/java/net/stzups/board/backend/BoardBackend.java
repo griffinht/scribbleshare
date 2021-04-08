cfbaedc298da2492633926c9528cd70899b210fe
stzups <lemonpickles1558@gmail.com>
2021-04-07 22:17:19 -0400

    fix maven

--- board-backend/src/main/java/net/stzups/board/backend/BoardBackend.java
+++ board-backend/src/main/java/net/stzups/board/backend/BoardBackend.java
@@ -1,6 +1,12 @@
 package net.stzups.board.backend;
 
+import net.stzups.board.util.LogFactory;
+
+import java.util.logging.Logger;
+
 public class BoardBackend {
+    private static final Logger LOGGER = LogFactory.getLogger("BoardBackend");
+
     public static void main(String[] args) {
 
     }
