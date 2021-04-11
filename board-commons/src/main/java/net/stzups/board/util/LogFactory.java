086b98022709f1d8e6118cd49a821db8906004cf
stzups <lemonpickles1558@gmail.com>
2021-04-11 13:16:37 -0400

    improve logger format

--- board-commons/src/main/java/net/stzups/board/util/LogFactory.java
+++ board-commons/src/main/java/net/stzups/board/util/LogFactory.java
@@ -1,7 +1,8 @@
 package net.stzups.board.util;
 
-import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Date;
 import java.util.logging.ConsoleHandler;
 import java.util.logging.Formatter;
 import java.util.logging.Handler;
@@ -15,7 +16,7 @@ import java.util.logging.Logger;
 public class LogFactory {
     /**
      * Creates or finds a Java logger and sets formatting to the following:
-     * [hh:mm:ss] [name] [level (if not INFO)] message
+     * [yyyy-MM-dd] [hh:mm:ss] [name] [level (if not INFO)] message
      *
      * @param name the name of the logger to create or find
      * @return the created logger
@@ -27,14 +28,13 @@ public class LogFactory {
         logger.addHandler(handler);
 
         handler.setFormatter(new Formatter() {
+            private final SimpleDateFormat dateFormat = new SimpleDateFormat("[yyyy-MM-dd] [HH:mm:ss] ");
+
             @Override
             public String format(LogRecord record) {
-                String level = (record.getLevel() == Level.INFO) ? "" : "[" + record.getLevel() + "] ";
-                return "["
-                        + new SimpleDateFormat("HH:mm:ss").format(new Timestamp(System.currentTimeMillis()))
-                        + "] ["
-                        + name
-                        + "] "
+                String level = (record.getLevel() == Level.INFO) ? "" : "[" + record.getLevel() + "] "; // include Logger Level if it is not Level.INFO
+                return  dateFormat.format(Date.from(Instant.now()))
+                        + "[" + name + "] "
                         + level
                         + record.getMessage()
                         + System.lineSeparator();
