566ba7fe28ef390545b16425a550323535cb4e3d
stzups <lemonpickles1558@gmail.com>
2021-04-25 10:05:48 -0400

    actually remove mouse_move

--- scribbleshare-room/src/main/java/net/stzups/scribbleshare/room/server/websocket/MessageHandler.java
+++ scribbleshare-room/src/main/java/net/stzups/scribbleshare/room/server/websocket/MessageHandler.java
@@ -14,7 +14,6 @@ import net.stzups.scribbleshare.room.server.websocket.protocol.client.ClientMess
 import net.stzups.scribbleshare.room.server.websocket.protocol.client.messages.ClientMessageCanvasUpdate;
 import net.stzups.scribbleshare.room.server.websocket.protocol.client.messages.ClientMessageDeleteDocument;
 import net.stzups.scribbleshare.room.server.websocket.protocol.client.messages.ClientMessageHandshake;
-import net.stzups.scribbleshare.room.server.websocket.protocol.client.messages.ClientMessageMouseMove;
 import net.stzups.scribbleshare.room.server.websocket.protocol.client.messages.ClientMessageOpenDocument;
 import net.stzups.scribbleshare.room.server.websocket.protocol.client.messages.ClientMessageUpdateDocument;
 import net.stzups.scribbleshare.room.server.websocket.protocol.server.messages.ServerMessageAddUser;
@@ -22,7 +21,6 @@ import net.stzups.scribbleshare.room.server.websocket.protocol.server.messages.S
 import net.stzups.scribbleshare.room.server.websocket.protocol.server.messages.ServerMessageDeleteDocument;
 import net.stzups.scribbleshare.room.server.websocket.protocol.server.messages.ServerMessageGetInvite;
 import net.stzups.scribbleshare.room.server.websocket.protocol.server.messages.ServerMessageHandshake;
-import net.stzups.scribbleshare.room.server.websocket.protocol.server.messages.ServerMessageMouseMove;
 import net.stzups.scribbleshare.room.server.websocket.protocol.server.messages.ServerMessageUpdateDocument;
 
 public class MessageHandler extends SimpleChannelInboundHandler<ClientMessage> {
@@ -119,10 +117,6 @@ public class MessageHandler extends SimpleChannelInboundHandler<ClientMessage> {
             }
             case READY: {
                 switch (message.getMessageType()) {
-                    case MOUSE_MOVE: {
-                        room.queueMessageExcept(new ServerMessageMouseMove(client, ((ClientMessageMouseMove) message).getMouseMoves()), client);
-                        break;
-                    }
                     case CANVAS_UPDATE: {
                         CanvasUpdate[] canvasUpdates = ((ClientMessageCanvasUpdate) message).getCanvasUpdates();
                         room.getCanvas().update(canvasUpdates);
