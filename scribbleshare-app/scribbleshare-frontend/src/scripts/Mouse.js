c2ea4f24f769abf815f7e897a8be7b1ecea9b7ad
stzups <lemonpickles1558@gmail.com>
2021-04-26 10:08:10 -0400

    fix right click to drag

--- scribbleshare-app/scribbleshare-frontend/src/scripts/Mouse.js
+++ scribbleshare-app/scribbleshare-frontend/src/scripts/Mouse.js
@@ -13,7 +13,7 @@ export default class Mouse {
             this.y = event.offsetY;
             this.dx += event.movementX;
             this.dy += event.movementY;
-            if (this.down) {
+            if (this.down && (event.buttons & 1) === 1) {
                 this.drag = true;
             }
         });
