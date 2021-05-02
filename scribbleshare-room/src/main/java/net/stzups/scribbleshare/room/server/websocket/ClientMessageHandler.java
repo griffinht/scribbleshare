20957f1e322db1e81abdb9c1b248412e56110f93
stzups <lemonpickles1558@gmail.com>
2021-05-02 17:51:16 -0400

    make easier to read

--- scribbleshare-room/src/main/java/net/stzups/scribbleshare/room/server/websocket/ClientMessageHandler.java
+++ scribbleshare-room/src/main/java/net/stzups/scribbleshare/room/server/websocket/ClientMessageHandler.java
@@ -59,156 +59,169 @@ public class ClientMessageHandler extends SimpleChannelInboundHandler<ClientMess
 
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, ClientMessage message) throws ClientMessageException {
-        switch (state) {
-            case HANDSHAKE: {
-                switch (message.getMessageType()) {
-                    case HANDSHAKE: {
-                        ClientMessageHandshake clientPacketHandshake = (ClientMessageHandshake) message;
-                        User user = ScribbleshareRoom.getDatabase().getUser(ctx.channel().attr(HttpAuthenticator.USER).get());
-                        if (user == null) {
-                            throw new ClientMessageException(message, "User does not exist");
-                        }
-                        ServerInitializer.getLogger(ctx).info("Handshake with invite " + clientPacketHandshake.getCode() + ", " + user);
-
-                        state = State.READY;
-                        client = new Client(user, ctx.channel());
-                        client.queueMessage(new ServerMessageHandshake(client));
-                        InviteCode inviteCode = ScribbleshareRoom.getDatabase().getInviteCode(clientPacketHandshake.getCode());
-                        client.queueMessage(new ServerMessageAddUser(client.getUser()));
-                        //figure out which document to open first
-                        if (inviteCode != null) {
-                            Document document = ScribbleshareRoom.getDatabase().getDocument(inviteCode.getDocument());
-                            if (document != null) {
-                                //if this isn't the user's own document and this isn't part of the user's shared documents then add and update
-                                if (document.getOwner() != client.getUser().getId()) {
-                                    if (client.getUser().getSharedDocuments().add(document.getId())) {
-                                        ScribbleshareRoom.getDatabase().updateUser(client.getUser());
-                                    }
-                                }
-                                try {
-                                    room = Room.getRoom(document);
-                                } catch (DeserializationException e) {
-                                    throw new ClientMessageException(message, e);
-                                }
-                            } else {
-                                throw new ClientMessageException(message, "Somehow used invite code for non existent document");
-                            }
-                        } else {
-                            if (client.getUser().getOwnedDocuments().size() == 0) {
-                                ScribbleshareRoom.getDatabase().createDocument(client.getUser());
-                            }
-                        }
-                        client.getUser().getOwnedDocuments().removeIf((id) -> {
-                            Document document = ScribbleshareRoom.getDatabase().getDocument(id);
-                            if (document == null) {
-                                return true;
-                            } else {
-                                client.queueMessage(new ServerMessageUpdateDocument(ScribbleshareRoom.getDatabase().getDocument(id)));
-                                return false;
-                            }
-                        });//todo this is bad
-                        client.getUser().getSharedDocuments().removeIf((id) -> {
-                            Document document = ScribbleshareRoom.getDatabase().getDocument(id);
-                            if (document == null) {
-                                return true;
-                            } else {
-                                client.queueMessage(new ServerMessageUpdateDocument(ScribbleshareRoom.getDatabase().getDocument(id)));
-                                return false;
-                            }
-                        });
-                        client.flushMessages();
-                        break;
-                    }
-                    default:
-                        throw new UnsupportedOperationException("Unsupported message type " + message.getMessageType() + " for state " + state);
-                }
-                break;
+        try {
+            switch (state) {
+                case HANDSHAKE:
+                    handleHandshakeState(ctx, message);
+                    break;
+                case READY:
+                    handleReadyState(ctx, message);
+                    break;
+                default:
+                    throw new ClientMessageException(message, "Can't handle message while in unknown state " + state);
             }
-            case READY: {
-                switch (message.getMessageType()) {
-                    case CANVAS_UPDATE: {
-                        CanvasUpdates[] canvasUpdates = ((ClientMessageCanvasUpdate) message).getCanvasUpdatesArray();
+        } catch (IndexOutOfBoundsException e) {
+            throw new ClientMessageException(message, e);
+        }
+
+    }
+
+    private void handleHandshakeState(ChannelHandlerContext ctx, ClientMessage message) throws ClientMessageException, IndexOutOfBoundsException {
+        switch (message.getMessageType()) {
+            case HANDSHAKE: {
+                ClientMessageHandshake clientPacketHandshake = (ClientMessageHandshake) message;
+                User user = ScribbleshareRoom.getDatabase().getUser(ctx.channel().attr(HttpAuthenticator.USER).get());
+                if (user == null) {
+                    throw new ClientMessageException(message, "User does not exist");
+                }
+                ServerInitializer.getLogger(ctx).info("Handshake with invite " + clientPacketHandshake.getCode() + ", " + user);
+
+                state = State.READY;
+                client = new Client(user, ctx.channel());
+                client.queueMessage(new ServerMessageHandshake(client));
+                InviteCode inviteCode = ScribbleshareRoom.getDatabase().getInviteCode(clientPacketHandshake.getCode());
+                client.queueMessage(new ServerMessageAddUser(client.getUser()));
+                //figure out which document to open first
+                if (inviteCode != null) {
+                    Document document = ScribbleshareRoom.getDatabase().getDocument(inviteCode.getDocument());
+                    if (document != null) {
+                        //if this isn't the user's own document and this isn't part of the user's shared documents then add and update
+                        if (document.getOwner() != client.getUser().getId()) {
+                            if (client.getUser().getSharedDocuments().add(document.getId())) {
+                                ScribbleshareRoom.getDatabase().updateUser(client.getUser());
+                            }
+                        }
                         try {
-                            room.getCanvas().update(canvasUpdates);
-                        } catch (CanvasUpdateException e) {
+                            room = Room.getRoom(document);
+                        } catch (DeserializationException e) {
                             throw new ClientMessageException(message, e);
                         }
-                        room.queueMessageExcept(new ServerMessageCanvasUpdate(canvasUpdates), client);
-                        break;
+                    } else {
+                        throw new ClientMessageException(message, "Somehow used invite code for non existent document");
                     }
-                    case OPEN_DOCUMENT: {
-                        ClientMessageOpenDocument clientPacketOpenDocument = (ClientMessageOpenDocument) message;
-                        Document document = ScribbleshareRoom.getDatabase().getDocument(clientPacketOpenDocument.getId());
-                        if (document != null) {
-                            if (room != null) {
-                                room.removeClient(client);
-                            }
-                            try {
-                                room = Room.getRoom(document);
-                            } catch (DeserializationException e) {
-                                throw new ClientMessageException(message, e);
-                            }
-                            room.addClient(client);
-                        } else {
-                            ServerInitializer.getLogger(ctx).warning(client + " tried to open document not that does not exist");
-                        }
-                        break;
+                } else {
+                    if (client.getUser().getOwnedDocuments().size() == 0) {
+                        ScribbleshareRoom.getDatabase().createDocument(client.getUser());
                     }
-                    case CREATE_DOCUMENT: {
-                        if (room != null) {
-                            room.removeClient(client);
-                        }
-                        try {
-                            room = Room.getRoom(ScribbleshareRoom.getDatabase().createDocument(client.getUser()));
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
-                        client.sendMessage(new ServerMessageUpdateDocument(room.getDocument()));
-                        room.addClient(client);
-                        break;
+                }
+                client.getUser().getOwnedDocuments().removeIf((id) -> {
+                    Document document = ScribbleshareRoom.getDatabase().getDocument(id);
+                    if (document == null) {
+                        return true;
+                    } else {
+                        client.queueMessage(new ServerMessageUpdateDocument(ScribbleshareRoom.getDatabase().getDocument(id)));
+                        return false;
                     }
-                    case DELETE_DOCUMENT: {
-                        ClientMessageDeleteDocument clientMessageDeleteDocument = (ClientMessageDeleteDocument) message;
-                        if (clientMessageDeleteDocument.getId() == room.getDocument().getId()) {
-                            ServerInitializer.getLogger(ctx).info("Deleting live document " + room.getDocument());
-                            room.sendMessage(new ServerMessageDeleteDocument(room.getDocument()));
-                            room.end();
-                            ScribbleshareRoom.getDatabase().deleteDocument(room.getDocument());
-                            break;
-                        } else {
-                            throw new ClientMessageException(message, "Tried to delete document which is not currently open");
-                        }
-/*                        Document document = ScribbleshareRoom.getDatabase().getDocument(clientMessageDeleteDocument.getId());
-                        if (document == null) {
-                            throw new MessageException(message, "Tried to delete document with id " + clientMessageDeleteDocument.getId() + " that does not exist");
-                        }
-                        if (document.getOwner() != client.getUser().getId()) {
-                            throw new MessageException(message, "Tried to delete document with id " + document.getId() +" which they do not own");
-                        }
-                        ServerInitializer.getLogger(ctx).info("Deleting dead document " + room.getDocument());
-                        ScribbleshareRoom.getDatabase().deleteDocument(room.getDocument());*/
-                        //Room.getRoom(document);
-                        //break;//todo better update logic
+                });//todo this is bad
+                client.getUser().getSharedDocuments().removeIf((id) -> {
+                    Document document = ScribbleshareRoom.getDatabase().getDocument(id);
+                    if (document == null) {
+                        return true;
+                    } else {
+                        client.queueMessage(new ServerMessageUpdateDocument(ScribbleshareRoom.getDatabase().getDocument(id)));
+                        return false;
                     }
-                    case UPDATE_DOCUMENT: {
-                        ClientMessageUpdateDocument clientMessageUpdateDocument = (ClientMessageUpdateDocument) message;
-                        if (clientMessageUpdateDocument.getName().length() > 64) {
-                            throw new ClientMessageException(message, "Tried to change name to string that is too long (" + clientMessageUpdateDocument.getName().length() + ")");
-                        }
-                        room.getDocument().setName(clientMessageUpdateDocument.getName());
-                        room.queueMessageExcept(new ServerMessageUpdateDocument(room.getDocument()), client);
-                        ScribbleshareRoom.getDatabase().updateDocument(room.getDocument());
-                        break;//todo better update logic
+                });
+                client.flushMessages();
+                break;
+            }
+            default:
+                throw new ClientMessageException(message, "Unsupported message for state " + state);
+        }
+    }
+
+    private void handleReadyState(ChannelHandlerContext ctx, ClientMessage message) throws ClientMessageException, IndexOutOfBoundsException {
+        switch (message.getMessageType()) {
+            case CANVAS_UPDATE: {
+                CanvasUpdates[] canvasUpdates = ((ClientMessageCanvasUpdate) message).getCanvasUpdatesArray();
+                try {
+                    room.getCanvas().update(canvasUpdates);
+                } catch (CanvasUpdateException e) {
+                    throw new ClientMessageException(message, e);
+                }
+                room.queueMessageExcept(new ServerMessageCanvasUpdate(canvasUpdates), client);
+                break;
+            }
+            case OPEN_DOCUMENT: {
+                ClientMessageOpenDocument clientPacketOpenDocument = (ClientMessageOpenDocument) message;
+                Document document = ScribbleshareRoom.getDatabase().getDocument(clientPacketOpenDocument.getId());
+                if (document != null) {
+                    if (room != null) {
+                        room.removeClient(client);
                     }
-                    case GET_INVITE: {
-                        client.sendMessage(new ServerMessageGetInvite(ScribbleshareRoom.getDatabase().getInviteCode(room.getDocument())));
-                        break;
+                    try {
+                        room = Room.getRoom(document);
+                    } catch (DeserializationException e) {
+                        throw new ClientMessageException(message, e);
                     }
-                    default:
-                        throw new UnsupportedOperationException("Unsupported message type " + message.getMessageType() + " for state " + state + " sent by " + client);
+                    room.addClient(client);
+                } else {
+                    ServerInitializer.getLogger(ctx).warning(client + " tried to open document not that does not exist");
                 }
                 break;
             }
+            case CREATE_DOCUMENT: {
+                if (room != null) {
+                    room.removeClient(client);
+                }
+                try {
+                    room = Room.getRoom(ScribbleshareRoom.getDatabase().createDocument(client.getUser()));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                client.sendMessage(new ServerMessageUpdateDocument(room.getDocument()));
+                room.addClient(client);
+                break;
+            }
+            case DELETE_DOCUMENT: {
+                ClientMessageDeleteDocument clientMessageDeleteDocument = (ClientMessageDeleteDocument) message;
+                if (clientMessageDeleteDocument.getId() == room.getDocument().getId()) {
+                    ServerInitializer.getLogger(ctx).info("Deleting live document " + room.getDocument());
+                    room.sendMessage(new ServerMessageDeleteDocument(room.getDocument()));
+                    room.end();
+                    ScribbleshareRoom.getDatabase().deleteDocument(room.getDocument());
+                    break;
+                } else {
+                    throw new ClientMessageException(message, "Tried to delete document which is not currently open");
+                }
+/*                        Document document = ScribbleshareRoom.getDatabase().getDocument(clientMessageDeleteDocument.getId());
+                if (document == null) {
+                    throw new MessageException(message, "Tried to delete document with id " + clientMessageDeleteDocument.getId() + " that does not exist");
+                }
+                if (document.getOwner() != client.getUser().getId()) {
+                    throw new MessageException(message, "Tried to delete document with id " + document.getId() +" which they do not own");
+                }
+                ServerInitializer.getLogger(ctx).info("Deleting dead document " + room.getDocument());
+                ScribbleshareRoom.getDatabase().deleteDocument(room.getDocument());*/
+                //Room.getRoom(document);
+                //break;//todo better update logic
+            }
+            case UPDATE_DOCUMENT: {
+                ClientMessageUpdateDocument clientMessageUpdateDocument = (ClientMessageUpdateDocument) message;
+                if (clientMessageUpdateDocument.getName().length() > 64) {
+                    throw new ClientMessageException(message, "Tried to change name to string that is too long (" + clientMessageUpdateDocument.getName().length() + ")");
+                }
+                room.getDocument().setName(clientMessageUpdateDocument.getName());
+                room.queueMessageExcept(new ServerMessageUpdateDocument(room.getDocument()), client);
+                ScribbleshareRoom.getDatabase().updateDocument(room.getDocument());
+                break;//todo better update logic
+            }
+            case GET_INVITE: {
+                client.sendMessage(new ServerMessageGetInvite(ScribbleshareRoom.getDatabase().getInviteCode(room.getDocument())));
+                break;
+            }
+            default:
+                throw new ClientMessageException(message, "Unsupported message for state " + state);
         }
     }
 }
