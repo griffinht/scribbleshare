148cea243db65ed52bb48b47a2e74a32d87be8ad
stzups <lemonpickles1558@gmail.com>
2021-05-25 10:09:31 -0400

    remove unused AuthenticationException.java

--- scribbleshare-commons/src/main/java/net/stzups/scribbleshare/data/objects/authentication/AuthenticationException.java
+++
@@ -1,7 +0,0 @@
-package net.stzups.scribbleshare.data.objects.authentication;
-
-public class AuthenticationException extends Exception {
-    public AuthenticationException(String message) {
-        super(message);
-    }
-}
