01a7d17574fbd7b4065e9c647a84a1ae996a60ad
stzups <lemonpickles1558@gmail.com>
2021-05-28 18:05:54 -0400

    microoptimization

--- scribbleshare-commons/src/main/java/net/stzups/scribbleshare/server/http/handler/RequestHandler.java
+++ scribbleshare-commons/src/main/java/net/stzups/scribbleshare/server/http/handler/RequestHandler.java
@@ -20,9 +20,11 @@ public abstract class RequestHandler extends HttpHandler {
     @Override
     public boolean handle(ChannelHandlerContext ctx, FullHttpRequest request) throws HttpException {
         String referer = request.headers().get(HttpHeaderNames.REFERER);
-        String string = (config.getSSL() ? "https://" : "http://") + config.getDomain();
-        if (referer != null && !referer.equals(string + this.referer) && !referer.equals(string + ":" + config.getPort() + this.referer)) { // https://example.com/referer or https://example.com:1234/referer
-            throw new UnauthorizedException("Bad referer for " + getRoute() + ", got " + HttpHeaderNames.REFERER + ": " + referer + ", should have been referred from " + this.referer);
+        if (referer != null) {
+            String string = (config.getSSL() ? "https://" : "http://") + config.getDomain();
+            if (!referer.equals(string + this.referer) && !referer.equals(string + ":" + config.getPort() + this.referer)) { // https://example.com/referer or https://example.com:1234/referer
+                throw new UnauthorizedException("Bad referer for " + getRoute() + ", got " + HttpHeaderNames.REFERER + ": " + referer + ", should have been referred from " + this.referer);
+            }
         }
         handleRequest(ctx, request);
         return true;
